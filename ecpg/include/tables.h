4b08874c5a5fd4ee98bb82c7088c49e6514d63bf
Sovary <kongsovaryhrdi@gmail.com>
2016-02-03 14:39:25 +0700

    Revert "Content management"
    This reverts commit 01a374ad899f591de096ae02aa5c6a1cf2da6b5e.

--- ecpg/include/tables.h
+++
@@ -1,7 +0,0 @@
-typedef struct{
-   int cont_id;
-   char cont_title [10+1] ;//=malloc(sizeof(cont_title))	;
-   char cont_des [10+1];
-   char cont_date [10+1];
-}CONT_T;
-#define CONT_SIZE (sizeof(CONT_T))
